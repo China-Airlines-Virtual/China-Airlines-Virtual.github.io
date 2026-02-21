a0bb7b771b4b1553f97f101ab43a741d2b0a1203
Green <andy199310@gmail.com>
2026-02-22 00:16:39 +0800

    Add 20260221 group flight map

--- assets/js/map-replay.js
+++ assets/js/map-replay.js
@@ -29,9 +29,10 @@ function initMapReplay(targetElementId, timelineName, center, zoom, members) {
         }
     ).setView(center, zoom);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 19
     }).addTo(map);
 
     const markerMap = new Map();
@@ -312,10 +313,11 @@ function initLiveMap(targetElementId, center, zoom) {
         fullscreenControl: true,
     }).setView(center, zoom);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        maxZoom: 19
     }).addTo(map);
+
 
     return map
 }
